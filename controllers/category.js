ac6cccbb7510600b469356058716eb7b2a1f3217
nawraahmed <nawra.alhaji@gmail.com>
2024-09-28 18:49:58 +0300

    add color code to each category

--- controllers/category.js
+++ controllers/category.js
@@ -41,3 +41,4 @@ exports.category_index_get = (req, res) => {
         })
     
 }
+
--- models/Category.js
+++ models/Category.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
     name: String,
+    color: String,
     event: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Event',
