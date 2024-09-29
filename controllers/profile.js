d2826d620f629c80cc7b4042f82f9ae436abbc14
Mujtaba18 <mujtab189@gmail.com>
2024-09-29 16:37:15 +0300

    Profile Page ongoing

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,7 +4,7 @@ exports.profile_get = (request, respond) => {
     // respond.send("Welcome to Blog App!");
     // respond.render("home/index", {welcomeMessage: "Welcome to My Blog App!!"});
     respond.render('profile/detail', {
-        welcomeMessage: request.user ? `Welcome ${request.user.name} to Crowd Crafter!!` : "Welcome to Crowd Crafter Event Planner!",
+        welcomeMessage: request.user ? `Welcome ${request.user.name} to your profile page!!` : "Welcome to Crowd Crafter Event Planner!",
         user: request.user
     });
 };
